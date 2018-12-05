d2e4e75c6a1eddedfb7c1807ebd1291d4d064f47
adhesivestiky <44936240+adhesivestiky@users.noreply.github.com>
2018-12-05 15:07:36 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,23 @@ const Discord = require("discord.js");
 const bot = new Discord.Client({disableEveryone: true});
 
 bot.on('ready', () => {
-  console.log('Bot is on');
+ console.log('Bot is on');
+  
 });
+
+bot.on("message", async message => {
+ if(message.author.bot) return;
+ if(!message.content.startsWith("$")) return;
+  };
+       
+let messageArray = message.content.split(" ");
+let cmd = messageArray[0];
+let args = messageArray.slice(1);
+
+if(cmd === '$ping'){
+ let m = await message.channel.send('Pinging');
+ m.edit(`Pong! \`${m.createdTimestamp - message.createdTimestamp}ms\``)
+};
+
 
 bot.login(process.env.token);
