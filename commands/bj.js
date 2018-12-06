2de98d03f25a03c9a353f15cda62ba3267c26f9e
adhesivestiky <44936240+adhesivestiky@users.noreply.github.com>
2018-12-05 21:06:23 -0800

    Update bj.js

--- commands/bj.js
+++ commands/bj.js
@@ -1,21 +1,32 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
- let playerlimit = args[0]
+ let playerlimit = args[0] + 1 - 1
+ if(playerlimit = NaN){
+  playerlimit = 3
+ };
+ 
  let a = new Discord.RichEmbed()
  .setColor('GREEN')
  .setDescription(`Players:\nPlayer limit: ${playerlimit}`);
  
  let b = await message.channel.send(a);
- b.react('🍤');
+ b.react('<:GoldenCasino:520091757036175370>');
  let c = message.channel.fetchMessage(`${b.id}`);
  
  let d = new Discord.RichEmbed()
  .setColor('GREEN')
- .setDescription(`Players: ${b.users}\nPlayer limit: ${playerlimit}`);
- if(b.count === playerlimit - 1){
+ .setDescription(`Players: ${b.fetchUsers(playerlimit - 1)}>\nPlayer limit: ${playerlimit}`);
+ 
+ let timer = new Set();
+ let time = 45
+ timer.add
+ setTimeout(() => {
+  timer.delete
+ } time * 1000);
+ 
   b.edit(d);
- }
+
  
 }
 
