bcad01b7acd199bf101e39b8851e329cf93d18d6
adhesivestiky <44936240+adhesivestiky@users.noreply.github.com>
2018-12-05 16:24:27 -0800

    Create eval.js

---
+++ commands/eval.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+  
+ const nice = ['306287412437450753', '425094586520829988'];
+  if (nice.includes(message.author.id)) {
+     try{
+      let code = args.join(" ");
+      let evaled = eval(code);
+
+      if(typeof evaled !== "string")
+      evaled = require("util").inspect(evaled);
+
+    message.channel.send(evaled);
+
+    } catch(err) {
+    message.channel.send(`\`\`\`Error\`\`\` \`\`\`x1\n${(err)}\n\`\`\``);
+    }
+    
+  }
+  
+}
+
+module.exports.help = {
+  name: "eval"
+}
