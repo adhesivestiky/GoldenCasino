002c41f697cd3b2ec0524794ea78fd1151bf3ee8
adhesivestiky <44936240+adhesivestiky@users.noreply.github.com>
2018-12-06 07:56:49 -0800

    Update bj.js

--- commands/bj.js
+++ commands/bj.js
@@ -8,7 +8,7 @@ module.exports.run = async (bot, message, args) => {
  
  let a = new Discord.RichEmbed()
  .setColor('GREEN')
- .setDescription(`Players:\nPlayer limit: ${playerlimit}\n**The game will start in 45 seconds**`);
+ .setDescription(`Players:\nPlayer limit: ${playerlimit}\n**The game will start in 45 seconds. React to join!**`);
  
  let b = await message.channel.send(a);
  b.react('🎲');
@@ -16,7 +16,7 @@ module.exports.run = async (bot, message, args) => {
  
  let d = new Discord.RichEmbed()
  .setColor('GREEN')
- .setDescription(`Players: ${b.users}>\nPlayer limit: ${playerlimit}`);
+ .setDescription(`Game has begun!`);
  
  let timer = new Set();
  let time = 45
@@ -25,7 +25,23 @@ module.exports.run = async (bot, message, args) => {
   timer.delete
   b.edit(d);
  }, time * 1000);
- 
+     message.channel.send(`please say a valid blackjack move`).then(() => {
+            message.channel.awaitMessages(response => response.content === `hit` || response.content === 'stay' || response.content === 'double' || response.content === 'fold',  {
+                max: 1, // number of responses to collect
+                time: 10000, //time that bot waits for answer in ms
+                errors: ['time'],
+            })
+                .then((collected) => {
+                    let play = collected.first().content; //this is the first response collected
+                    message.channel.send('You chose to ' + play);
+                   if(play == 'fold'){
+                    message.channel.send('Ok, you quit');
+                   }
+                })
+                .catch(() => { // if no message is collected
+                    message.channel.send('I didnt catch that, Try again.');
+                });
+        });
 
  
 }
