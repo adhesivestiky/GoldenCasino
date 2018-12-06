84908814a582d0882471e02cc48ebf96328a8d69
adhesivestiky <44936240+adhesivestiky@users.noreply.github.com>
2018-12-05 16:50:42 -0800

    Update warn.js

--- commands/warn.js
+++ commands/warn.js
@@ -2,10 +2,18 @@ const Discord = require("discord.js");
 //any other things, such as JSONs and fs
 
 module.exports.run = async (bot, message, args) => {
+ if(!message.author.hasPermission('MANAGE_MESSAGES')) return message.channel.reply('You can\'t use this command!');
  let member = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
  let reason = args[1];
  
- member.send(reason);
+ let embed = new Discord.RichEmbed()
+ .setColor('RANDOM')
+ .setDescription(`<@${member.id}> has been warned./nReason: ${reason}.`)
+ .setFooter(`Warned by ${message.author.tag}`, message.author.displayAvatarURL)
+ .setTimestamp();
+ 
+ message.channel.send(embed)
+ member.send(`You have been warned in ${message.guild.name} because of ${reason}.`);
  
 };
 
