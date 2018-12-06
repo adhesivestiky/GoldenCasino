f8ab35a628894194f3586d306ca7a87f1699e101
adhesivestiky <44936240+adhesivestiky@users.noreply.github.com>
2018-12-05 16:13:21 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,45 @@
+const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 const bot = new Discord.Client({disableEveryone: true});
+const fs = require("fs");
+bot.commands = new Discord.Collection();
 
-bot.on('ready', () => {
- console.log('Bot is on');
- bot.user.setActivity(`you`) 
+fs.readdir("./commands/", (err, files) => {
+
+  if(err) console.log(err);
+
+  let jsfile = files.filter(f => f.split(".").pop() === "js")
+  if(jsfile.length <= 0){
+    console.log("couldn't find commands");
+    return;
+  }
+
+  jsfile.forEach((f, i) =>{
+    let props = require(`./commands/${f}`);
+    console.log(`${f} loaded...`);
+    bot.commands.set(props.help.name, props);
+
+  });
+});
+
+
+
+bot.on("ready", () => {
+  console.log(`${bot.user.username} is online on these servers: ${bot.guilds.map(g => g.name)}!!`);
+  bot.user.setActivity(`With ${bot.users.size} users | .help`);
 });
 
 bot.on("message", async message => {
   if(message.author.bot) return;
-  if(!message.content.startsWith("$")) return;
+  if(!message.content.startsWith("." || "<@520003742691622921> ")) return;
 
-       
- let messageArray = message.content.split(" ");
- let cmd = messageArray[0];
- let args = messageArray.slice(1);
+  let prefix = [".", "<@520003742691622921> "];
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
 
- if(cmd === '$ping'){
-  let m = await message.channel.send('Pinging');
-  m.edit(`Pong! \`${m.createdTimestamp - message.createdTimestamp}ms\``)
- };
- 
+  let commandfile = bot.commands.get(cmd.slice(prefix.length));
+  if(commandfile) commandfile.run(bot,message,args);
 });
-
 
 bot.login(process.env.token);
