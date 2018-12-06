4f9e3853ce7f651c4e1136b87fbda441d86f0adb
adhesivestiky <44936240+adhesivestiky@users.noreply.github.com>
2018-12-05 17:21:52 -0800

    Update bj.js

--- commands/bj.js
+++ commands/bj.js
@@ -4,11 +4,15 @@ module.exports.run = async (bot, message, args) => {
  let playerlimit = args[0];
  let a = new Discord.RichEmbed()
  .setColor('GREEN')
- .setDescription(`Players: ${a.users.username.join(', ')}`);
+ .setDescription(`Players:\nPlayer limit: ${args[0]}`);
  
  let b = await message.channel.send(a);
  b.react('🍤');
  let c = msg.channel.fetchMessage(`${b.id}`);
+ 
+ let d = new Discord.RichEmbed()
+ .setColor('GREEN')
+ .setDescription(`Players: ${a.users.username.join(", ")}\nPlayer limit: ${args[0]}`);
  
  if(c.count == playerlimit){
   b.edit(a)
