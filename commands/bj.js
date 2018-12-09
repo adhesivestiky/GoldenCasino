d2ca0993fc47a7d78a0eed6f54519598ca4a55a4
adhesivestiky <44936240+adhesivestiky@users.noreply.github.com>
2018-12-08 20:03:36 -0800

    Update bj.js

--- commands/bj.js
+++ commands/bj.js
@@ -8,7 +8,7 @@ module.exports.run = async (bot, message, args) => {
  
  let a = new Discord.RichEmbed()
  .setColor('GREEN')
- .setDescription(`Players:\nPlayer limit: ${playerlimit}\n**The game will start in 45 seconds. React to join!**`);
+ .setDescription(`Players:\nPlayer limit: ${playerlimit}\n**The game will start in 20 seconds. React to join!**`);
  
  let b = await message.channel.send(a);
  b.react('🎲');
@@ -18,43 +18,88 @@ module.exports.run = async (bot, message, args) => {
  .setColor('GREEN')
  .setDescription(`Game has begun!`);
  
+ 
+ let rand1 = Math.floor(Math.random() * 12);
+ let rand2 = Math.floor(Math.random() * 3);
+ var card;
+ var type;
+ 
+ if(rand1 = 0){
+  card = 'Ace'
+ } else if(rand1 = 1){
+  card = '2'
+ } else if(rand1 = 2){
+  card = '3'
+ } else if(rand1 = 3){
+  card = '4'
+ } else if(rand1 = 4){
+  card = '5'
+ } else if(rand1 = 5){
+  card = '6'
+ } else if(rand1 = 6){
+  card = '7'
+ } else if(rand1 = 7){
+  card = '8'
+ } else if(rand1 = 8){
+  card = '9'
+ } else if(rand1 = 9){
+  card = '10'
+ } else if(rand1 = 10){
+  card = 'King'
+ } else if(rand1 = 11){
+  card = 'Queen'
+ } else {
+  card = 'Jack'
+ };
+ 
+ if(rand2 = 0){
+  type = 'of spades'
+ } else if(rand2 = 1){
+  type = 'of diamonds'
+ } else if(rand2 = 2){
+  type = 'of clubs'
+ } else {
+  type = 'of hearts'
+ };
+ 
  let timer = new Set();
- let time = 45
+ let time = 20
  timer.add
  setTimeout(() => {
   timer.delete
   b.edit(d);
- }, time * 1000);
+  
+  message.channel.send(`Please say a valid blackjack move.`).then(() => {	
+  message.channel.awaitMessages(response => response.content === `hit` || response.content === 'stay' || response.content === 'double' || response.content === 'fold',  {	
+             max: 1, // number of responses to collect	
+             time: 10000, //time that bot waits for answer in ms	
+             errors: ['time'],	
+         })	
+             .then((collected) => {	
+                 let play = collected.first().content.toLowerCase(); //this is the first response collected	
+                 message.channel.send('You chose to ' + play);	
+                if(play == 'fold'){	
+                 message.channel.send('Ok, you quit');	
+                }	
+            
+            if(play == 'hit'){
+              message.channel.send('Drawing another card.. Jk I haven\'t bought any cards yet lol');
+            };
+             
+          if(play == 'stay'){
+            message.channel.send('No.')
+          }
+             
+          if(play == 'double'){
+           message.channel.send('Why would I double your bet of 0?');
+          }
+             })	
+             .catch(() => { // if no message is collected	
+                 message.channel.send('I didnt catch that, Try again.');	
+             });	
+     });
  
-     message.channel.send(`Please say a valid blackjack move.`).then(() => {	
-            message.channel.awaitMessages(response => response.content === `hit` || response.content === 'stay' || response.content === 'double' || response.content === 'fold',  {	
-                max: 1, // number of responses to collect	
-                time: 10000, //time that bot waits for answer in ms	
-                errors: ['time'],	
-            })	
-                .then((collected) => {	
-                    let play = collected.first().content.toLowerCase(); //this is the first response collected	
-                    message.channel.send('You chose to ' + play);	
-                   if(play == 'fold'){	
-                    message.channel.send('Ok, you quit');	
-                   }	
-             
-               if(play == 'hit'){
-                 message.channel.send('Drawing another card.. Jk I haven\'t bought any cards yet lol');
-               };
-             
-             if(play == 'stay'){
-               message.channel.send('No.')
-             }
-             
-             if(play == 'double'){
-              message.channel.send('Why would I double your bet of 0?');
-             }
-                })	
-                .catch(() => { // if no message is collected	
-                    message.channel.send('I didnt catch that, Try again.');	
-                });	
-        });
+ }, time * 1000);
  
 }
 
