fa04bc4bcb0c8ad3e3af25f7e2fde8ca7150a890
adhesivestiky <44936240+adhesivestiky@users.noreply.github.com>
2018-12-13 20:55:24 -0800

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -2,8 +2,8 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-let chan = args[0];
-chan.send(args.join(" "));
+
+message.channel.send(args.join(" "));
 
 message.delete();
 }
