871ca9e1b005b6812179d585e7123187a957878e
adhesivestiky <44936240+adhesivestiky@users.noreply.github.com>
2018-12-13 19:58:37 -0800

    Update watch.js

--- commands/watch.js
+++ commands/watch.js
@@ -1,9 +1,21 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
- let spychan = args[1];
+ let a = 'b';
+ let stop = message.channel.fetchMessage('522985414726909954');
+ while(a == 'b'){
+  let spychan = args[0];
+  let messages = new Discord.MessageCollector()
+  .channel(spychan)
+  .filter(args);
+   
+  message.channel.send(messages);
+  if(stop.count == 2){
+   break;
+  };
+ };
 }
 
 module.exports.help = {
- name: ""
+ name: "watch"
 }
