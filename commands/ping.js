33e12d42a636abc08b5136f470fdca21413334b3
adhesivestiky <44936240+adhesivestiky@users.noreply.github.com>
2018-12-13 18:37:35 -0800

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,7 +2,18 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
  let m = await message.channel.send("Pinging...");
- m.edit(`Pong! \`${m.createdTimestamp - message.createdTimestamp}ms\``)
+ m.edit(`Loading embed...`);
+ m.delete();
+ let ping = Math.round(bot.ping);
+ 
+ let pingembed = new Discord.RichEmbed()
+ .setColor('RANDOM')
+ .setTitle('Pong!', 'https://cdn.discordapp.com/attachments/516507779738107919/522964461804584960/pangedr.gif')
+ .setDescription(`**Response Time:** \`${m.createdTimestamp - message.createdTimestamp}ms\`\n**Bot Ping:** \`${ping}ms\``)
+ .setFooter(`Panged by ${message.author.tag}`, message.author.displayAvatarURL)
+ .setTimestamp();
+ 
+ message.channel.send(pingembed);
 }
 
 module.exports.help = {
