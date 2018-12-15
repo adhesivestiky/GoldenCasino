40f9029a25496c21810ee35cb19178b85b48c628
adhesivestiky <44936240+adhesivestiky@users.noreply.github.com>
2018-12-14 18:31:11 -0800

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -12,8 +12,9 @@ module.exports.run = async (bot, message, args) => {
       evaled = require("util").inspect(evaled);
 
     let gembed = new Discord.RichEmbed()
-    .setTitle('Evaled!', 'https://cdn.discordapp.com/attachments/519728995202236416/523324536460935168/green.png')
-    .setDescription(`\`${evaled}\``)
+    .setTitle('Evaled!')
+    .setColor('#41f44d')
+    .setDescription(`\`\`\`${evaled}\`\`\``)
     .setFooter(`Evaled by ${message.author.tag}`, message.author.displayAvatarURL)
     .setTimestamp();
        
@@ -22,8 +23,8 @@ module.exports.run = async (bot, message, args) => {
     } catch(err) {
       let errembed = new Discord.RichEmbed()
       .setColor('#f44242')
-      .setAuthor('Error!', 'https://cdn.discordapp.com/attachments/519728995202236416/523324195216818176/red-40144_1280.png')
-      .setDescription(`Error:x1\n${(err)}`)
+      .setTitle('Error!')
+      .setDescription(`Error:\n\`\`\`${(err)}\`\`\``)
       .setFooter(`Eval ran by ${message.author.tag}`, message.author.displayAvatarURL)
       .setTimestamp();
  
