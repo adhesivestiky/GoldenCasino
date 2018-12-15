4f61009b1bc9a71cb0d2ebfc6c29b67e37207dc8
adhesivestiky <44936240+adhesivestiky@users.noreply.github.com>
2018-12-15 09:26:35 -0800

    Update stickytext.js

--- commands/stickytext.js
+++ commands/stickytext.js
@@ -1,7 +1,9 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-  if(message.author.hasPermission('MANAGE_MESSAGES'))
+  if(!message.author.hasPermission('MANAGE_MESSAGES')){
+    message.channel.send('Sorry, you can\'t use this.');
+  };
   let smessage = args.join(" ");
   let m = await message.channel.send(smessage);
   if(message){
