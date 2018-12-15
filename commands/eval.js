e0a2be1f0e31a470ccc8cb5bde3173d6f9323e8f
adhesivestiky <44936240+adhesivestiky@users.noreply.github.com>
2018-12-14 18:27:05 -0800

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
   
- const nice = ['306287412437450753', '425094586520829988'];
+ const nice = ['306287412437450753', '425094586520829988', '421931065423167508'];
   if (nice.includes(message.author.id)) {
      try{
       let code = args.join(" ");
@@ -11,10 +11,23 @@ module.exports.run = async (bot, message, args) => {
       if(typeof evaled !== "string")
       evaled = require("util").inspect(evaled);
 
-    message.channel.send(evaled);
+    let gembed = new Discord.RichEmbed()
+    .setTitle('Evaled!', 'https://cdn.discordapp.com/attachments/519728995202236416/523324536460935168/green.png')
+    .setDescription(`\`${evaled}\``)
+    .setFooter(`Evaled by ${message.author.tag}`, message.author.displayAvatarURL)
+    .setTimestamp();
+       
+    message.channel.send(gembed);
 
     } catch(err) {
-    message.channel.send(`\`\`\`Error\`\`\` \`\`\`x1\n${(err)}\n\`\`\``);
+      let errembed = new Discord.RichEmbed()
+      .setColor('#f44242')
+      .setAuthor('Error!', 'https://cdn.discordapp.com/attachments/519728995202236416/523324195216818176/red-40144_1280.png')
+      .setDescription(`Error:x1\n${(err)}`)
+      .setFooter(`Eval ran by ${message.author.tag}`, message.author.displayAvatarURL)
+      .setTimestamp();
+ 
+    message.channel.send(errembed);
     }
     
   }
