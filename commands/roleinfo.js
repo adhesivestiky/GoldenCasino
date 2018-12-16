773018724832614dd11225fa20c971db2f50c3b0
adhesivestiky <44936240+adhesivestiky@users.noreply.github.com>
2018-12-15 17:57:01 -0800

    Create roleinfo.js

---
+++ commands/roleinfo.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js");
+//any other things, such as JSONs and fs
+
+module.exports.run = async (bot, message, args) => {
+ //Your code here
+}
+
+module.exports.help = {
+ name: "roleinfo"
+}
