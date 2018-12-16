13d31f22f1c452fd5df00fd454e6bf7c65b15bf5
adhesivestiky <44936240+adhesivestiky@users.noreply.github.com>
2018-12-15 17:40:27 -0800

    Update stickytext.js

--- commands/stickytext.js
+++ commands/stickytext.js
@@ -1,17 +1,24 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-  if(!message.member.hasPermission('MANAGE_MESSAGES')){
-    message.channel.send('Sorry, you can\'t use this.');
-  };
-  let smessage = args.join(" ");
-  let m = await message.channel.send(smessage);
-  if(message){
-    m.delete();
-    message.channel.send(m);
+  let supertimer2000 = 'on'
+  while(supertimer2000 = 'on'){
+    if(!message.member.hasPermission('MANAGE_MESSAGES')){
+      message.channel.send('Sorry, you can\'t use this.');
+    };
+    let smessage = args.join(" ");
+    let m = await message.channel.send(smessage);
+    if(message){
+      m.delete();
+      message.channel.send(smessage);
+    };
+    
+    if(message.content == '$stickyremove'){
+      break;
+    };
   };
  
-  message.channel.send("I've sent a message that will always stay at the bottom of the chat! To remove it, delete the message!");
+  message.channel.send("I've sent a message that will always stay at the bottom of the chat! To remove it, say `$stickyremove`!");
 
 }
 
